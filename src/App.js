991c48988e87fdaf7cf903561567a3e38b2b3093
Mikael Scientia <mikaelmuniz2001@gmail.com>
2021-06-14 13:54:09 -0300

    updated!

--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
-const query = "harry potter";
+
+const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
  
-function App() {
-  const [data, setData] = useState({ items: [], volumeInfo: "", id: ""});
-  
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        `https://www.googleapis.com/books/v1/volumes?q=${query}`,
-      );
-      setData(result.data);
+      setIsError(false);
+      setIsLoading(true);
+ 
+      try {
+        const result = await axios(url);
+ 
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+ 
+      setIsLoading(false);
     };
+ 
     fetchData();
-  }, []);
+  }, [url]);
+ 
+  return [{ data, isLoading, isError }, setUrl];
+};
+function App() {
+  const [query, setQuery] = useState('redux');
+  const [{ data, isLoading, isError}, doFetch] = useDataApi (
+    `https://www.googleapis.com/books/v1/volumes?q=${query}`,
+    { books: [] },
+  )
  
   return (
-    <ul>
-      {data.items.map(book => (
-        <li>
-          <a key={book.items}>{book.volumeInfo.title}</a>
-          <a key={book.items}>{book.id}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <form
+        onSubmit={event => {
+          doFetch(
+            `https://www.googleapis.com/books/v1/volumes?q=${query}`,
+          );
+ 
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+ 
+      {isError && <div>Something went wrong ...</div>}
+ 
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {data.books.map(book => (
+            <li>
+              <a>{book}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Fragment>
   );
 }
  
